d744712646602e99e3dadcf2ecceed6f575c9002
oumaimabenamara <benamaraaoumaymaa98@gmail.com>
2020-12-21 15:44:35 +0100

    first commit

---
+++ Projet.js
@@ -0,0 +1,13 @@
+// SELECTORS
+
+// EVENTLISTENERS
+
+// FUNCTIONS
+
+
+// localStorage.setItem('user', 'bechir');
+// console.log(localStorage.getItem('user'));
+
+// const user= {name:'bechir', age:18};
+// localStorage.setItem('user', JSON.stringify(user));
+// console.log(JSON.parse(localStorage.getItem('user')));
---
+++ README.md
@@ -0,0 +1,1 @@
+"# SCRATCH-MAP" 
