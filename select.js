23f7db5cf31122eb7122bf1a47c61df7504f1036
Thebechir <bechirnaouar2@gmail.com>
2020-12-23 14:52:59 +0100

    select country

---
+++ select.js
@@ -0,0 +1,150 @@
+// declarations
+const countries = document.querySelectorAll('.LP');
+const visiter = document.querySelector('#CathegoriePaysA');
+const Avisiter= document.querySelector('#CathegoriePaysB');
+const Arevisiter= document.querySelector('#CathegoriePaysC');
+
+const countrieschecked = JSON.parse(localStorage.getItem('countrieschecked')) || [];
+
+
+// event listeners
+// document.addEventListener("submit",optionChecked)
+// document.addEventListener("submit",optionStored)
+
+
+
+
+// functions
+
+
+// function save(){
+//     console.log(countries);
+//     var checkbox = document.getElementById('argentina');
+//     //localStorage.setItem('argentina', checkbox.checked);
+//     console.log(document.getElementById('argentina').value);
+// }
+
+// function load(){    
+//     var checked = JSON.parse(localStorage.getItem('argentina'));
+//     document.getElementById("argentina").checked = checked;
+// }
+
+// function wis(){
+//     location.reload();
+//     localStorage.clear()
+
+// }
+
+// load();
+// console.log(document.getElementById('argentina').value);
+
+
+// if (checked=1) {
+//     console.log(checked);
+    
+// }
+// else{
+//     push
+// }
+let array = []
+let CP = {
+    array1:[],
+    array2:[],
+    array3:[],
+}
+
+// let clicked1 = (document.querySelector("CathegoriePaysA").clicked == true);
+// let clicked2 = (document.querySelector("CathegoriePaysB").clicked == true);
+// let clicked3 = (document.querySelector("CathegoriePaysC").clicked == true);
+
+
+
+
+
+function C1(){
+     CP.array1 = [...array, ...CP.array1]
+     array = []
+     console.log(CP)
+}
+
+
+function C2(){
+    CP.array2 = array
+}
+
+
+function C3(){
+    CP.array3 = array
+}
+
+
+// let array1 =[];
+const containerbig = document.querySelector('.LP-container');
+containerbig.addEventListener('click',e=> 
+{
+    if (e.target.classList.contains('LP')) 
+    {
+        
+        if (e.target.checked) 
+        {
+array.push(e.target.value);
+
+            // if (clicked1) 
+            // {
+            //     CP.array1.push(e.target.value);
+            // }
+            // else if (clicked2) 
+            // {
+            //     CP.array2.push(e.target.value);
+            // }
+            // else if (clicked3) 
+            // {
+            //     CP.array3.push(e.target.value);
+            // }
+        }
+            
+        
+        else
+        {
+            array=array.filter(element => element!=  e.target.value)
+            // if (array1.includes(e.target.value)) 
+            // {
+            //     array1=array1.filter(element => element!=  e.target.value);  
+            // }
+            // else if (array2.includes(e.target.value)) 
+            // {
+            //     array2=array2.filter(element => element!=  e.target.value);   
+            // }
+            // else if (array3.includes(e.target.value)) 
+            // {
+            //     array3=array3.filter(element => element!=  e.target.value); 
+            // }
+        }
+        // console.log(array1);
+    }
+})
+
+visiter.addEventListener('click', C1);
+Avisiter.addEventListener('click', C2);
+Arevisiter.addEventListener('click', C3);
+
+
+// countries.forEach(country=>{
+//     country.addEventListener('click',e=>{
+        
+//         console.log(e.target.checked);
+
+//         if (e.target.checked) {
+//             array1.push(e.target.value)
+
+            
+//         }else{
+//             array1=array1.filter(element => element!=  e.target.value)
+
+//         }
+//         console.log(array1);
+//     })
+// })
+
+
+
